a6754614fe3996fc35b98c93666bb00a76849fb7
DrChinaWhite <drhawkins@mta.ca>
2021-09-05 14:02:48 -0400

    Add files via upload

---
+++ WS06/TextFile.h
@@ -0,0 +1,52 @@
+//==============================================
+// Name:           Dale Hawkins
+// Student Number: #########
+// Email:          @myseneca.ca
+// Section:        OOP244-NBB
+// Workshop:       6 (LAB)
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+//==============================================
+
+#ifndef SDDS_TEXTFILE_H__
+#define SDDS_TEXTFILE_H__
+#include <iostream>
+namespace sdds {
+   class Line {
+      char* m_value{ nullptr };
+      operator const char* ()const;
+      Line() {}
+      Line& operator=(const char*);
+      ~Line();
+      friend class TextFile;
+      // copy and copy assignment prevention goes here
+
+   };
+
+   class TextFile {
+      Line* m_textLines{ nullptr };
+      char* m_filename{ nullptr };
+      unsigned m_noOfLines;
+      unsigned m_pageSize;
+      void setFilename(const char* fname, bool isCopy = false);
+      void setNoOfLines();
+      void loadText();
+      void saveAs(const char *fileName)const;
+      void setEmpty();
+   public:
+      TextFile(unsigned pageSize = 15);
+      TextFile(const char* filename, unsigned pageSize = 15);
+      TextFile(const TextFile&);
+      TextFile& operator=(const TextFile&);
+      ~TextFile();
+      std::ostream& view(std::ostream& ostr)const;
+      std::istream& getFile(std::istream& istr);
+      operator bool()const;
+      unsigned lines()const;
+      const char* name()const;
+      const char* operator[](unsigned index)const;
+   };
+   std::ostream& operator<<(std::ostream& ostr, const TextFile& text);
+   std::istream& operator>>(std::istream& istr, TextFile& text);
+
+}
+#endif // !SDDS_TEXTFILE_H__
