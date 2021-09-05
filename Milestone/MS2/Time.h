01bf0227fadf007d62fba32fb399160a5462beed
DrChinaWhite <drhawkins@mta.ca>
2021-09-05 14:23:18 -0400

    Add files via upload

---
+++ Milestone/MS2/Time.h
@@ -0,0 +1,41 @@
+//==============================================
+// Name:           Dale Hawkins
+// Student Number: #########
+// Email:          @myseneca.ca
+// Section:        OOP244-NBB
+// Workshop:       1 (MS)
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+//==============================================
+
+#ifndef SDDS_TIMER_H
+#define SDDS_TIMER_H
+#include <iostream>
+namespace sdds {
+   class Time {
+      unsigned int m_min;
+   public:
+      Time& setToNow();
+      Time(unsigned int min = 0);
+      std::ostream& write(std::ostream& ostr) const;
+      std::istream& read(std::istream& istr);
+
+      Time& operator-=(const Time& D);
+      Time operator-(const Time& D)const;
+      Time& operator+=(const Time& D);
+      Time operator+(const Time& D)const;
+
+      Time& operator=(unsigned int val);
+      Time& operator *= (unsigned int val);
+      Time& operator /= (unsigned int val);
+      Time operator *(unsigned int val)const;
+      Time operator /(unsigned int val)const;
+
+      operator int()const;
+      operator unsigned int()const;
+   };
+   std::ostream& operator<<(std::ostream& ostr, const Time& D);
+   std::istream& operator>>(std::istream& istr, Time& D);
+}
+
+#endif // !SDDS_TIME_H
+
